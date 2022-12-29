c9648926d8a720c41cef8353da05c4c04b7e438d
RaviVishnu <mail2ravi20@gmail.com>
2022-12-29 14:59:43 +0530

    Test123

---
+++ src/org/insta/Insta.java
@@ -0,0 +1,29 @@
+package org.insta;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Insta {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver",
+				"H:\\Selenium\\Greens\\Workspace\\Selenium Day3 task\\Amazon\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		
+		Duration duration = Duration.ofSeconds(100);
+		driver.manage().timeouts().implicitlyWait(duration);
+		
+		driver.get("https://www.instagram.com/accounts/login/");
+
+		//Thread.sleep(4000);
+	
+		driver.findElement(By.name("username")).sendKeys("12345678");
+		
+	}
+
+}
